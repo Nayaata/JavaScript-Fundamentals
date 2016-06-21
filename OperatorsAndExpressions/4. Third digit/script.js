1e1d537be6ba5f58ffcd60bd80d587d581b8ff45
NayaIT <n.m.keranova@gmail.com>
2016-06-21 16:50:38 +0300

    Upload Operators And Expressions HW

---
+++ OperatorsAndExpressions/4. Third digit/script.js
@@ -0,0 +1,12 @@
+function solve(args) {
+    var num = +args[0];
+    var thirdDigit = Math.floor(num / 100) % 10;
+
+    if (thirdDigit === 7){
+        console.log('true');
+    }
+    else{
+        console.log('false ' + thirdDigit)
+    }
+}
+
